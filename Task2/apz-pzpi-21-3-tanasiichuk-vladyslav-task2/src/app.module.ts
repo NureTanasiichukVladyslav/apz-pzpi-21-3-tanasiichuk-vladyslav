7c1af0755820e7e7ccef49fc951ff00996be1fb4
NureTanasiichukVladyslav <vladyslav.tanasiichuk@nure.ua>
2024-05-08 16:07:59 +0300

    Task2: add auth

--- Task2/apz-pzpi-21-3-tanasiichuk-vladyslav-task2/src/app.module.ts
+++ Task2/apz-pzpi-21-3-tanasiichuk-vladyslav-task2/src/app.module.ts
@@ -1,17 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PrismaModule } from './prisma';
+import { AuthModule } from './auth';
+import { UserModule } from './users';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    JwtModule.registerAsync({
+      global: true,
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UserModule,
     PrismaModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- Task2/apz-pzpi-21-3-tanasiichuk-vladyslav-task2/src/main.ts
+++ Task2/apz-pzpi-21-3-tanasiichuk-vladyslav-task2/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  app.use(cookieParser());
+  app.enableCors({ origin: 'http://localhost:3000/api#/', credentials: true });
 
   const config = new DocumentBuilder()
     .setTitle(
